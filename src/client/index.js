38fb2e2736d37cad31a18cab8bb96f6c1fce07ad
s0r01nv <sakethram.ramalingam@walmartlabs.com>
2020-02-07 10:18:09 -0800

    First js script

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 
 function App() {
-  return <h1>Hello, World</h1>;
+  return <h1>Good Morning jsDrome</h1>;
+
 }
 
 render(<App />, document.getElementById('root'));
