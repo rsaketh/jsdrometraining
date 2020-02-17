2c7eb057397284d9446218175027ce8230ed2973
s0r01nv <sakethram.ramalingam@walmartlabs.com>
2020-02-17 07:59:17 -0800

    React code

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-function App() {
-  return <h1>Good Morning jsDrome</h1>;
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
+  increment = () => {
+    const currentCount = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentCount + 1,
+    });
+  };
+
+  decrement = () => {
+    const currentCount = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentCount - 1,
+    });
+  };
+
+  render() {
+    return <div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.increment}>+</button>
+      <button onClick={this.decrement}>-</button>
+    </div>
+  }
 }
 
 render(<App />, document.getElementById('root'));
